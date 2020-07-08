90723619aba9e544c845fd2e02673a89cd480f29
Masanori Kado <kdmsnr@gmail.com>
2020-07-08 13:37:40 +0900

    update script

--- uml.rb
+++ uml.rb
@@ -1,18 +1,18 @@
 require "csv"
 
 essence = {
-  'stakeholders.csv' => 'class ステークホルダー',
   'opportunity.csv' => 'class 機会',
+  'stakeholders.csv' => 'class ステークホルダー',
   'requirements.csv' => 'class 要求',
   'software-system.csv' => 'class ソフトウェアシステム',
-  'team.csv' => 'enum チーム',
   'work.csv' => 'enum 作業',
+  'team.csv' => 'enum チーム',
   'way-of-working.csv' => 'enum 作業方法'
 }
 
 icons = {
-  'stakeholders.csv' => '',
   'opportunity.csv' => '',
+  'stakeholders.csv' => '',
   'requirements.csv' => '<< (S,orchid) >>',
   'software-system.csv' => '<< (S,orchid) >>',
   'team.csv' => '',
@@ -21,19 +21,22 @@ icons = {
 }
 
 puts "@startuml"
-# puts "scale 500 width"
 
 essence.keys.each do |csv|
   header = essence[csv]
   icon = icons[csv]
   open(csv) do |f|
+    state_num = 0
     while line = f.gets
       k, v = CSV.parse(line)[0]
-      if k.include? "□"
-        puts "  -#{v}"
-      else
-        puts "}" if f.lineno != 1
-        puts "#{header}.#{v} #{icon} {"
+      if !k.nil? and !v.nil?
+        if k.include? "□"
+          puts "  -#{v}"
+        else
+          state_num += 1
+          puts "}" if f.lineno != 1
+          puts "#{header}(#{state_num})：#{v} #{icon} {"
+        end
       end
     end
     puts "}"
